5b6053437fa0dabd0e4a875436af386fa58073b2
leonardo <leonardo123garavito@gmail.com>
2025-08-29 20:34:06 -0500

    CREACION DE PRODUCTOS EN BASE DE TADOS

--- database/bdatos.sql
+++ database/bdatos.sql
@@ -15,3 +15,26 @@ CREATE TABLE IF NOT EXISTS t_usuarios(
 )ENGINE=InnoDb;
 
 INSERT INTO t_usuarios VALUE(NULL, 'leonardo','garavito','leonardo123garavito@gmail.com','123456789','admin','imagen');
+
+CREATE TABLE IF NOT EXISTS t_categoria(
+    id_categoria  int(11)         auto_increment NOT NULL,
+    nombre_categoria      varchar(100)                   NOT NULL,
+    CONSTRAINT  pk_categoria      PRIMARY KEY(id_categoria)
+
+)ENGINE=InnoDb;
+
+INSERT INTO t_categoria VALUE(NULL, 'contruncion'),(NULL, 'electricos'),(NULL, 'pisos'),(NULL, 'pinturas');
+
+CREATE TABLE IF NOT EXISTS t_productos(
+    id_producto     int(11)         auto_increment NOT NULL,
+    nombre_producto varchar(100)                   NOT NULL,
+    descripcion     varchar(200)                   NOT NULL,
+    precio          float(10,9)                    NOT NULL,
+    stock           int(100)                       NOT NULL,
+    oferta          varchar(200)                   NOT NULL,
+    fecha           date                           NOT NULL,
+    imagen          varchar(255),
+    id_categoria    int(11)                        NOT NULL,
+    CONSTRAINT  pk_producto      PRIMARY KEY(id_producto),
+    CONSTRAINT  fk_categoria     FOREIGN KEY(id_producto) REFERENCES t_categoria(id_categoria)
+)ENGINE=InnoDb;
